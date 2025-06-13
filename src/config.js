b839d9a586b844aa5f8b75a66fb8c9023910737d
Morad <morad.zu20@gmail.com>
2025-06-13 22:44:09 +0300

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: 'https://ll01wvlzde.execute-api.eu-central-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://7besrm1wl4.execute-api.eu-central-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
